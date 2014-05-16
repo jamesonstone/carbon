8b5573e730200a0df844c006c4e5bb8a4c83356b
Jameson <jameson@voxy.com>
2014-05-16 17:27:28 -0400

    fixed up the tests; need to figure out setup/teardown

--- test_mobile_login.py
+++ test_mobile_login.py
@@ -10,21 +10,39 @@ PATH = lambda p: os.path.abspath(
     os.path.join(os.path.dirname(__file__), p)
 )
 
-class TestMobileLogin(unittest.TestCase):
-    def setUp(self):
-        desired_caps = {}
-        desired_caps['platformName'] = 'Android'
-        desired_caps['platformVersion'] = '4.2'
-        desired_caps['deviceName'] = 'Android Emulator'
-        desired_caps['app'] = PATH(
+driver = None
+
+def setUp():
+    desired_caps = {}
+    desired_caps['platformName'] = 'Android'
+    desired_caps['platformVersion'] = '4.2'
+    desired_caps['deviceName'] = 'Android Emulator'
+    desired_caps['app'] = PATH(
             '../../repo/android/app/build/apk/app-debug-unaligned.apk'
         )
-        self.driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
+    driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
+
+def tearDown():
+    # end the session
+    driver.quit()
 
 
-    def tearDown(self):
-        # end the session
-        self.driver.quit()
+
+
+class TestMobileLogin(  unittest.TestCase):
+#     def setUp(self):
+#     desired_caps = {}
+#     desired_caps['platformName'] = 'Android'
+#     desired_caps['platformVersion'] = '4.2'
+#     desired_caps['deviceName'] = 'Android Emulator'
+#     desired_caps['app'] = PATH(
+#             '../../repo/android/app/build/apk/app-debug-unaligned.apk'
+#         )
+#     self.driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
+
+# def tearDown(self):
+#     # end the session
+#     self.driver.quit()
 
     def test_a_sign_button(self):
         # TODO: change this explicit wait
@@ -35,8 +53,6 @@ class TestMobileLogin(unittest.TestCase):
         self.assertEquals('Sign in', el.text)
         print el.text
         el.click()
-
-    # def test_b_bad_login(self):
         #enter bad credientals 
         #verify errors
         print 'entering bad email and password'
@@ -47,6 +63,18 @@ class TestMobileLogin(unittest.TestCase):
         password = self.driver.find_element_by_id('com.voxy.news.debug:id/password')
         self.assertIsNotNone(password)
         password.send_keys('wowowowowowo')
+        self.driver.implicitly_wait(5)
+
+        signup = self.driver.find_element_by_id('com.voxy.news.debug:id/signup')
+        self.assertIsNotNone(signup)
+        self.driver.implicitly_wait(5)
+        print signup.text
+        signup.click()
+        self.driver.implicitly_wait(8)
+
+        alertmsg = self.driver.find_element_by_id('android:id/alertTitle')
+        self.assertIsNotNone(alertmsg)
+        self.assertEquals(alertmsg.text, 'Email or password incorrect')
 
 
 
