2e339809304642e2bb0eefb4af76a535659f0d02
Jameson <jameson@voxy.com>
2014-05-19 17:45:28 -0400

    module exploration

--- test_mobile_login.py
+++ test_mobile_login.py
@@ -1,101 +1,47 @@
 import os
 from time import sleep
-
+import android
 import unittest 
-
 from appium import webdriver
-
-# Returns abs path relative to this file and not cwd
-PATH = lambda p: os.path.abspath(
-    os.path.join(os.path.dirname(__file__), p)
-)
-
-# driver = None
-
-# def setUp():
-#     desired_caps = {}
-#     desired_caps['platformName'] = 'Android'
-#     desired_caps['platformVersion'] = '4.2'
-#     desired_caps['deviceName'] = 'Android Emulator'
-#     desired_caps['app'] = PATH(
-#             '../../repo/android/app/build/apk/app-debug-unaligned.apk'
-#         )
-#     driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
-
-# def tearDown():
-#     # end the session
-#     driver.quit()
-
-class Test(unittest.TestCase):
-    @classmethod     
-    def setUpClass(self):
-        desired_caps = {}
-        desired_caps['platformName'] = 'Android'
-        desired_caps['platformVersion'] = '4.2'
-        desired_caps['deviceName'] = 'Android Emulator'
-        desired_caps['app'] = PATH(
-                '../../repo/android/app/build/apk/app-debug-unaligned.apk'
-            )
-        self.driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
-
-    @classmethod
-    def tearDownClass(self):       
-        # end the session
-        self.driver.quit()
 
 
 class TestMobileLogin(unittest.TestCase):
-    # def setUp(self):
-    #     desired_caps = {}
-    #     desired_caps['platformName'] = 'Android'
-    #     desired_caps['platformVersion'] = '4.2'
-    #     desired_caps['deviceName'] = 'Android Emulator'
-    #     desired_caps['app'] = PATH(
-    #             '../../repo/android/app/build/apk/app-debug-unaligned.apk'
-    #         )
-    #     self.driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
-
-    # def tearDown(self):
-    #     # end the session
-    #     self.driver.quit()
-
-    def setUp(self):
-        self.
+    android.setUp()
 
     def test_a_sign_button(self):
         # TODO: change this explicit wait
-        self.driver.implicitly_wait(8)
+        sleep(10)
         print '\nwait 8 seconds'
-        el = self.driver.find_element_by_id('com.voxy.news.debug:id/login')
-        self.assertIsNotNone(el)
-        self.assertEquals('Sign in', el.text)
+        el = android.DRIVER.find_element_by_id('com.voxy.news.debug:id/login')
+        android.assertIsNotNone(el)
+        android.assertEquals('Sign in', el.text)
         print el.text()
         el.click()
         #enter bad credientals 
         #verify errors
         print 'entering bad email and password'
-        email = self.driver.find_element_by_id('com.voxy.news.debug:id/email')
-        self.assertIsNotNone(email)
+        email = android.DRIVER.find_element_by_id('com.voxy.news.debug:id/email')
+        android.assertIsNotNone(email)
         email.send_keys('voxy@boxyroxy.com')
 
-        password = self.driver.find_element_by_id('com.voxy.news.debug:id/password')
-        self.assertIsNotNone(password)
+        password = android.DRIVER.find_element_by_id('com.voxy.news.debug:id/password')
+        android.assertIsNotNone(password)
         password.send_keys('wowowowowowo')
-        self.driver.implicitly_wait(5)
+        sleep(5)
 
-        signup = self.driver.find_element_by_id('com.voxy.news.debug:id/signup')
-        self.assertIsNotNone(signup)
-        self.driver.implicitly_wait(5)
+        signup = android.DRIVER.find_element_by_id('com.voxy.news.debug:id/signup')
+        android.assertIsNotNone(signup)
+        sleep(5)
         print signup.text()
         signup.click()
-        self.driver.implicitly_wait(8)
+        #android.DRIVER.implicitly_wait(8)
+        sleep(5)
 
-        alertmsg = self.driver.find_element_by_id('android:id/alertTitle')
-        self.assertIsNotNone(alertmsg)
-        self.assertEquals(alertmsg.text, 'Email or password incorrect')
+        alertmsg = android.DRIVER.find_element_by_id('android:id/alertTitle')
+        android.assertIsNotNone(alertmsg)
+        android.assertEquals(alertmsg.text, 'Email or password incorrect')
 
-    Test.tearDownClass()
-
+    android.tearDown()
 
 
 
