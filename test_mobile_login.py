2eb656247dc6fa6dbeb582b02218eed06e1d9e06
Jameson <jameson@voxy.com>
2014-05-21 15:48:16 -0400

    got things working

--- test_mobile_login.py
+++ test_mobile_login.py
@@ -4,54 +4,26 @@ import android
 import unittest 
 from appium import webdriver
 
-
-# def setUpModule():
-#     android.setUp()
-
-# def tearDownModule():
-#     android.tearDown()
-
+global login_button
 
 
 class TestMobileLogin(unittest.TestCase):
+    def setUp(self):
+        self.driver = android.setUp()
 
-    def test_a_sign_button(self):
-        DRIVER = android.setUp()
+    def tearDown(self):
+        android.tearDown()
 
-        # TODO: change this explicit wait
-        #DRIVER.implicitly_wait(8)
-        sleep(8)
-        print '\nwait 8 seconds'
-        
-        el = android.DRIVER.find_element_by_id('com.voxy.news.debug:id/login')
-        self.assertIsNotNone(el)
-        self.assertEquals('Sign in', el.text)
-        print el.text()
-        el.click()
-        
-        #enter bad credientals 
-        #verify errors
-        print 'entering bad email and password'
-        email = android.DRIVER.find_element_by_id('com.voxy.news.debug:id/email')
-        assertIsNotNone(email)
-        email.send_keys('voxy@boxyroxy.com')
+    def test_login(self):
+        # TODO: change this implicity wait
+        self.driver.implicitly_wait(8)
+        login_button = self.driver.find_element_by_id('com.voxy.news.debug:id/login')
+        self.assertIsNotNone(login_button)
 
-        password = androd.DRIVER.find_element_by_id('com.voxy.news.debug:id/password')
-        assertIsNotNone(password)
-        password.send_keys('wowowowowowo')
-        DRIVER.implicitly_wait(8)
+    def test_login_button(self):
+        login_button = self.driver.find_element_by_id('com.voxy.news.debug:id/login')
+        login_button.click()
 
-        signup = DRIVER.find_element_by_id('com.voxy.news.debug:id/signup')
-        assertIsNotNone(signup)
-        DRIVER.implicitly_wait(8)
-        print signup.text()
-
-        signup.click()
-        DRIVER.implicitly_wait(8)
-
-        alertmsg = DRIVER.find_element_by_id('android:id/alertTitle')
-        assertIsNotNone(alertmsg)
-        assertEquals(alertmsg.text, 'Email or password incorrect')
 
 
 
@@ -59,3 +31,47 @@ class TestMobileLogin(unittest.TestCase):
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(TestMobileLogin)
     unittest.TextTestRunner(verbosity=2).run(suite)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    #enter bad credientals 
+        # #verify errors
+        # print 'entering bad email and password'
+        # email = android.DRIVER.find_element_by_id('com.voxy.news.debug:id/email')
+        # assertIsNotNone(email)
+        # email.send_keys('voxy@boxyroxy.com')
+
+        # password = androd.DRIVER.find_element_by_id('com.voxy.news.debug:id/password')
+        # assertIsNotNone(password)
+        # password.send_keys('wowowowowowo')
+        # DRIVER.implicitly_wait(8)
+
+        # signup = DRIVER.find_element_by_id('com.voxy.news.debug:id/signup')
+        # assertIsNotNone(signup)
+        # DRIVER.implicitly_wait(8)
+        # print signup.text()
+
+        # signup.click()
+        # DRIVER.implicitly_wait(8)
+
+        # alertmsg = DRIVER.find_element_by_id('android:id/alertTitle')
+        # assertIsNotNone(alertmsg)
+        # assertEquals(alertmsg.text, 'Email or password incorrect')
