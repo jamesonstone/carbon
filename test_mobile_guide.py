f34e82fa652c17eda9d5c8193fa53653f12341e4
Jameson <jameson@voxy.com>
2014-05-27 17:51:55 -0400

    added more scolling and text checks

--- test_mobile_guide.py
+++ test_mobile_guide.py
@@ -19,9 +19,17 @@ class TestMobileGuide(unittest.TestCase):
 		android.driver.implicitly_wait(10)
 		self.assertIsNotNone(android.driver.find_element_by_id('com.voxy.news.debug:id/title'))
 		self.assertIsNotNone(android.driver.find_element_by_id('com.voxy.news.debug:id/goalsTitle'))
-		android.driver.execute_script("mobile: swipe", {"touchCount": 1 , "startX": 698, "startY": 776,
-		 "endX": 251, "endY": 779, "duration": 0.9256836 })
-		sleep(5)
+		android.driver.execute_script("mobile: swipe", {"touchCount": 1 , "startX": 693,
+		 "startY": 818, "endX": 224, "endY": 833, "duration": 1.216719 })
+		sleep(3)
+		android.driver.execute_script("mobile: swipe", {"touchCount": 1 ,
+		 "startX": 718, "startY": 778, "endX": 239, "endY": 781, "duration": 1.201836 })
+		sleep(3)
+		android.driver.execute_script("mobile: swipe", {"touchCount": 1 ,
+		 "startX": 118, "startY": 820, "endX": 531, "endY": 824, "duration": 1.307949 })
+		sleep(3)
+		self.assertIsNotNone(android.driver.find_element_by_id('com.voxy.news.debug:id/snapshot'))
+		self.assertIsNotNone(android.driver.find_element_by_id('com.voxy.news.debug:id/currentLevel'))
 
 
 
